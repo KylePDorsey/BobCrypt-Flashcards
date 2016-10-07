679c65bc428188c50cb6fa0d2f5faaffa4e7a4c8
Kyle Dorsey & Rachael Lunney & Sayam Kim <pair+kylepdorsey.rakaloo.sayamkim@devbootcamp.com>
2016-10-07 18:11:49 -0500

    Add rounds/:id logic and guessed_cards association to Round model

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -1,9 +1,16 @@
 get '/rounds/:id' do
-
+  @round = Round.find(params[:id])
+  @cards = Card.where(deck_id: @round.deck_id)
+  @guessed_cards = @round.guessed_cards
 end
 
 post '/rounds' do
-
+  @round = Round.new(deck_id: params[:deck_id])
+  if logged_in?
+    @round.user = current_user
+  end
+  @round.save
+  redirect "/rounds/#{@round.id}"
 end
 
 get 'rounds/:id/complete' do
--- app/models/round.rb
+++ app/models/round.rb
@@ -3,6 +3,7 @@ class Round < ActiveRecord::Base
   belongs_to :user
   belongs_to :deck
   has_many :guesses
+  has_many :guessed_cards, through: :guesses, source: :card
 
   validates :deck_id, :presence => true
 
