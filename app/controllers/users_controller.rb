73c9082ac305bede1137010e25cc643a18478a91
Matthew Galloway & Nick Pentella <pair+mwgalloway.npentella@devbootcamp.com>
2016-10-07 16:00:28 -0500

    Setup user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 get '/users/new' do
   erb :"users/new"
 end
+
+get "/users/login" do
+  erb :'users/login'
+end
+
+
+
+post '/users/login' do
+  @user = User.authenticate(params['email'], params['password'])
+  if @user
+    session[:id] = @user.id
+    redirect '/'
+  else
+    @errors = ["Invalid credentials"]
+    erb :"users/login"
+  end
+end
+
+post '/users' do
+  User.create(params['user'])
+  redirect '/'
+end
