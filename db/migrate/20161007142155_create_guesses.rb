037c32dcc66fbec7def50555cfcc6b8920c9fa40
Kyle Dorsey <KylePDorsey@gmail.com>
2016-10-07 14:38:44 -0500

    Write initial migrations

---
+++ db/migrate/20161007142155_create_guesses.rb
@@ -0,0 +1,10 @@
+class CreateGuesses < ActiveRecord::Migration
+  def change
+    create_table :guesses do |t|
+      t.integer :card_id
+      t.string :potential_answer
+
+      t.timestamps(null: false)
+    end
+  end
+end
