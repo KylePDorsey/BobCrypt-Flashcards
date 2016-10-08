e77415adfac7b1f7baf40cb2fc1b38debad0a3bd
Matthew Galloway <email": null>
2016-10-08 15:54:21 -0500

    Lint and refactor rounds controller and round helpers

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -1,21 +1,21 @@
 get '/rounds/:id' do
   @round = Round.find(params[:id])
   @cards = Card.where(deck_id: @round.deck_id)
-  guessed_cards = @round.guessed_cards
-  @potential_cards = @cards - guessed_cards
-  begin_next_pass_if_empty
 
+  @potential_cards = @cards - @round.guessed_cards
+
+  begin_next_pass_if_empty
   @potential_cards += cards_answered_incorrectly_this_pass
-
   begin_next_pass_if_empty
+
   redirect "/rounds/#{@round.id}/complete"
 end
 
 post '/rounds' do
   @round = Round.new(deck_id: params[:deck_id])
-  if logged_in?
-    @round.user = current_user
-  end
+
+  @round.user = current_user if logged_in?
+
   @round.save
   redirect "/rounds/#{@round.id}"
 end
--- app/helpers/round_helpers.rb
+++ app/helpers/round_helpers.rb
@@ -1,9 +1,9 @@
 helpers do
   def begin_next_pass_if_empty
-    if @potential_cards.length > 0
-      card_to_display = @potential_cards.sample
-      redirect "/rounds/#{@round.id}/cards/#{card_to_display.id}"
-    end
+    return nil if @potential_cards.empty?
+
+    card_to_display = @potential_cards.sample
+    redirect "/rounds/#{@round.id}/cards/#{card_to_display.id}"
   end
 
   def cards_answered_incorrectly_this_pass
@@ -11,7 +11,4 @@ helpers do
       card.guesses.where(round_id: @round.id).last.incorrect?
     end
   end
-
-
-
 end
