ebd766d4472a1c1dd0baf34da4a10e30b9e3c3a9
Matthew Galloway <email": null>
2016-10-08 17:41:27 -0500

    Lint controller files

--- app/controllers/guesses.rb
+++ app/controllers/guesses.rb
@@ -1,7 +1,6 @@
 post '/guesses' do
- @guess = Guess.create(params['guess'])
- @round = Round.find(params['guess']['round_id'])
- @card = Card.find_by(id: params['guess']['card_id'])
- erb :'/cards/show_answer'
- # redirect "/rounds/#{round.id}"
+   @guess = Guess.create(params['guess'])
+   @round = Round.find(params['guess']['round_id'])
+   @card = Card.find_by(id: params['guess']['card_id'])
+   erb :'/cards/show_answer'
 end
