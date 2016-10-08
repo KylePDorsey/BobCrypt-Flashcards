4500a9bd257646a2106e5b0188a5d5fd62018a2f
Matthew Galloway <email": null>
2016-10-08 14:02:34 -0500

    Write players association for Deck model

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,6 +2,7 @@ class Deck < ActiveRecord::Base
   # Remember to create a migration!
   has_many :cards
   has_many :rounds
+  has_many :players, :through => :rounds, :source => :user
 
   validates :theme, :presence => true, :uniqueness => true
 end
